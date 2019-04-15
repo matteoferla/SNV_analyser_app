b9725fc877ec99ffcacdaf6dadc1a70f188b4993
matteoferla <matteo.ferla@gmail.com>
2019-04-15 17:13:45 +0100

    :bug: env variable wrongly declared

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from pyramid.paster import get_app
 import os
 
 if 'SQL_URL' in os.environ:  # postgres in production!
-    app = get_app('production.ini', 'main', sql_url=os.environ['SQL_URL'])
+    app = get_app('production.ini', 'main', options={'sql_url':os.environ['SQL_URL']})
 else:
     app = get_app('development.ini', 'main')
 serve(app, host='0.0.0.0', port=8000, threads=50)
